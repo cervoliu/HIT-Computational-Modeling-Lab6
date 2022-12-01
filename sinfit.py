d0ebfac9c089bfd15dd0c0fb9dfe0f9b93d844e4
cervoliu <72922098+cervoliu@users.noreply.github.com>
2022-12-01 22:18:04 +0800

    update outlier

--- sinfit.py
+++ sinfit.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_sin_data(n = 100, mu = 0, sigma = 0.1):
+def get_sin_data(n = 100, mu = 0, sigma = 0.1, dirt = 0, m = 10):
     """
         Draw n samples from y = sin(2 * pi * x) with Gaussian noise ~ N(mu, sigma),
-        where x in [0, 1)
+        where x in [0, 2),
+        then add m outliers, absolute offset in [0.3, 1)
 
         return [[x_0, y_0], [x_1, y_1], ..., [x_{n-1}, y_{n-1}]]
     """
-    x = np.random.uniform(0, 1, n)
-    x.sort()
-    y = np.sin(2 * np.pi * x) + np.random.normal(mu, sigma, n)
-    return np.array([x, y]).T
+    x = np.random.uniform(0, 2, n + m)
+    y = np.sin(2 * np.pi * x)
+    noise = np.random.normal(mu, sigma, n)
+    offset = np.random.choice([-1, 1], m) * np.random.uniform(0.3, 1, m)
+    z = y + np.concatenate((noise, offset))
+    data = np.array([x, z]).T
+    return data
 
-def fit(data, lam = 0, m = 15):
+def fit(data, lam = 0, m = 14):
     """
         Use least square method with L2 regularization coefficient
         lambda = lam (default = 0, i.e. without regularization) to fit data,
@@ -28,7 +32,7 @@ def fit(data, lam = 0, m = 15):
 def draw(data, w, title):
     train_x = data[:, 0]
     train_y = data[:, 1]
-    real_x = np.linspace(0, 1, 50)
+    real_x = np.linspace(0, 2, 100)
     real_y = np.sin(2 * np.pi * real_x)
     mat = np.array([real_x ** i for i in range(len(w))]).T
     fit_y = np.dot(mat, w)
